3d09d234fda5a7663f831bc20a859ea0fa25689f
lukusamurai <lukusamudiayi@gmail.com>
2025-11-29 10:31:14 +0200

    fixing download handlers ux +  meta tags

--- assets/js/contact-form-handler.js
+++ assets/js/contact-form-handler.js
@@ -1,10 +1,12 @@
 "use strict";
 
-const contactForm = document.getElementById("contact-form-container");        
+const contactForm = document.getElementById("contact-form-container");  
+const downloadsListWrapper = document.getElementById("downloads-container");       
 
 document.getElementById("contact-us").addEventListener("click", function(event) {  
-    event.preventDefault(); 
-    contactForm.style.display = "block";    
+    event.preventDefault();
+    downloadsListWrapper.style.display = "none";
+    contactForm.style.display = "block";      
 });
 
 document.getElementById("close").addEventListener("click", function(event) {
--- assets/js/downloads-handler.js
+++ assets/js/downloads-handler.js
@@ -1,10 +1,13 @@
 "use strict";
 
-const downloadsList = document.getElementById("downloads-container");        
+const downloadsList = document.getElementById("downloads-container");
+const contactFormWrapper = document.getElementById("contact-form-container");          
 
 document.getElementById("downloads").addEventListener("click", function(event) {  
     event.preventDefault(); 
+    contactFormWrapper.style.display = "none";
     downloadsList.style.display = "block";    
+
 });
 
 document.getElementById("close-downloads").addEventListener("click", function(event) {
