180bb3d2a3954b884a9239488dd39bd9bddcb2ee
lukusamurai <lukusamudiayi@gmail.com>
2025-12-04 11:45:54 +0200

    share buttons tweaks

--- assets/js/share-buttons-handler.js
+++ assets/js/share-buttons-handler.js
@@ -12,44 +12,10 @@ document.getElementById("share-button").addEventListener("mouseover", function(e
     }    
 });
 
-/*const clickableElements = document.querySelectorAll(".share-button");
+const clickableElements = document.querySelectorAll(".share-button");
 
 clickableElements.forEach(element => {
   element.addEventListener('click', function(event) {    
     shareButtons.style.display = "none";        
   });
-}); */
-
-
-/* ***** copying text - insights ***** */
-let insighstUrl = "https://l-u-k-u-s-a-m-u-r-a-i.github.io/insights.html";
-const insightsElements = document.querySelectorAll(".share-button");
-insightsElements.forEach(element => {
-  element.addEventListener('click', function(event) {  
-    navigator.clipboard.writeText(insighstUrl)
-    .then(() => {      		
-      		alert('Text copied to clipboard!');      		
-    	})
-    	.catch(err => {
-      		console.error('Failed to copy text: ', err);     		 
-		});  
-    shareButtons.style.display = "none";        
-  });
-});
-
-
-/* ***** copying text - glossary ***** */
-let glossaryUrl = "https://l-u-k-u-s-a-m-u-r-a-i.github.io/digital-transformation-glossary.html";
-const glossaryElements = document.querySelectorAll(".share-button-glossary");
-glossaryElements.forEach(element => {
-  element.addEventListener('click', function(event) {  
-    navigator.clipboard.writeText(glossaryUrl)
-    .then(() => {      		
-      		alert('Text copied to clipboard!');      		
-    	})
-    	.catch(err => {
-      		console.error('Failed to copy text: ', err);     		 
-		});  
-    shareButtons.style.display = "none";        
-  });
-});
+}); 
