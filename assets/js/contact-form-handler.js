89332691f0b7e85abfc6cb1b004b768db3853b6b
lukusamurai <lukusamudiayi@gmail.com>
2025-12-24 12:00:57 +0200

    ui/ux - home button flow & fix headings on service page

--- assets/js/contact-form-handler.js
+++ assets/js/contact-form-handler.js
@@ -1,16 +1,28 @@
 "use strict";
 
-const contactForm = document.getElementById("contact-form-container");  
 const downloadsListWrapper = document.getElementById("downloads-container");       
+
+window.addEventListener('load', (event) => {    
+    const queryString = window.location.search; 
+    const urlParams = new URLSearchParams(queryString);  
+    const action = urlParams.get('action-type');     
+    if (action !== undefined && action === "contact-form") {  
+        downloadsListWrapper.style.display = "block";  
+        downloadsListWrapper.scrollIntoView({ behavior: 'smooth', block: 'start' });  
+    };
+});
+
+const contactForm = document.getElementById("contact-form-container");  
 
 document.getElementById("contact-us").addEventListener("click", function(event) {  
     event.preventDefault();
     downloadsListWrapper.style.display = "none";
     contactForm.style.display = "block";  
-    contactForm.focus();    
+    contactForm.focus();
 });
 
 document.getElementById("close").addEventListener("click", function(event) {
     event.preventDefault(); 
     contactForm.style.display = "none";    
 });
+ 
